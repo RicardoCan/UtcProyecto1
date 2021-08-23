0373eaac12cba3226d89ceabc9ef8fa48b0de38a
Edwin Ek <isai_ek1@hotmail.com>
2021-08-23 16:15:09 -0500

    vistas adminLte docentes, administrador y estilos de inicio principal
    se agrego los CSS  faltates, se agregaron las rutas correspondientes y los adminlte de las 3 vistas disponibles

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Vista De Adminlte
+Route::view('administrador','layouts.adminlte');
+Route::view('docentes', 'layouts.adminltedocentes');
 
 //Vista De inicio principal
 Route::view('/','principal.principal');
+
+
+//Vista de inicio
+Route::view('bienvenida','layouts.inicio');
+Route::view('inicio','principal.principal');
+
+
+//Vista administrador
+
+Route::view('recurso','administrador.recursos.recursos');
+Route::view('home','administrador.home.home');
+Route::view('sala','administrador.salas.salas');
+
+//Vista docentes
+Route::view('configuracion','docentes.configuracion.configuracion');
+Route::view('solicitudes','docentes.historialsolicitudes.historialsolicituds');
+Route::view('panelsolicitudes','docentes.paneldesolicitudes.panelsolicituds');
+Route::view('solicitudrecurso','docentes.solicitudes.solicitudrecurso');
+Route::view('solicitudsala','docentes.solicitudes.solicitudsala');
+Route::view('calendario','docentes.calendario.calendario');
