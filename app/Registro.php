19cf3365809419d998b000626244f48d52578aae
RICARDO ANTONIO CAN MATEY <60706062+RicardoCan@users.noreply.github.com>
2021-08-23 19:43:06 -0500

    Creación De Modulo Completo Registro
    Creación de controlador, modelo, js, un master un layouts y una vista de registro

---
+++ app/Registro.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Registro extends Model
+{
+   // esta linea sirve para vincular el modelo con una tabla.
+    protected $table='users';
+
+    // se especifica la clave primaria.
+    protected $primaryKey='nick';
+
+    // cuando no sea un numero la clave primaria y sea un varchar poner el siguiente comando.
+    public $incrementing=false;
+
+    // desactiva las etiquetas de tiempo.
+    public $timestamps=false;
+
+    // definimos los campos que van a recivir valor de los que se pueden pedir por el usuario.
+    protected $fillable=[
+        'nick',
+        'password',
+        'nombres',
+        'apellidos',
+        'id_rol',
+        'active'
+   ];
+}
